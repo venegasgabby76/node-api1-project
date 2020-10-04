5fa883cdad8d90a5df281b780e4fcdd4b4557ef9
Gabriellia Venegas-Johnson <61035529+venegasgabby76@users.noreply.github.com>
2020-10-04 11:13:09 -0700

    finally finished the project.

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ server.get("/", (req, res) => {
 });
 
 server.post("/users", (req, res) => {
-    const newUser = db.createUser({
-        name: req.body.name,
-        bio: req.body.bio,
-    })
+  const newUser = db.createUser({
+    name: req.body.name,
+    bio: req.body.bio,
+  });
 
   if (!req.body.name || !req.body.bio) {
     return res.status(400).json({
@@ -26,23 +26,92 @@ server.post("/users", (req, res) => {
   if (newUser) {
     res.status(201).json(newUser);
   } else {
-      res.status(500).json({
-        errorMessage: "There was an error while saving the user to the database"
-      })
+    res.status(500).json({
+      errorMessage: "There was an error while saving the user to the database",
+    });
   }
 });
 
 server.get("/users", (req, res) => {
-    const users = db.getUsers();
+  const users = db.getUsers();
 
-    if (users) {
-        res.status(201).json(users)
-    } else {
-        res.status(500).json({
-            errorMessage: "The users information could not be retrieved."
-        })
-    }
-})
+  if (users) {
+    res.status(201).json(users);
+  } else {
+    res.status(500).json({
+      errorMessage: "The users information could not be retrieved.",
+    });
+  }
+});
+
+server.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+
+  const user = db.getUserById(id);
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({
+      message: "The user with the specified ID does not exist.",
+    });
+  }
+});
+
+server.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const user = db.getUserById(id);
+
+  if (user) {
+    db.deleteUser(id);
+    // 204 means a successful empty response
+    res.status(204).end();
+  } else {
+    res.status(404).json({
+     message: "The user with the specified ID does not exist.",
+    });
+  }
+});
+
+
+
+server.put("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const user = db.getUserById(id);
+
+  if (user) {
+    const upDateUser = db.updateUser(id, {
+      name: req.body.name,
+      bio: req.body.bio
+    });
+    res.json(upDateUser);
+  } else {
+    res.status(404).json({
+      message: "User not found",
+    });
+  }
+});
+
+// server.put("/users/:id", (req, res) => {
+//   const upDate = db.updateUser({
+//     name: req.body.name,
+//     bio: req.body.bio,
+//   });
+
+//   if (!req.body.name || !req.body.bio) {
+//     return res.status(400).json({
+//       errorMessage: "Please provide name and bio for the user.",
+//     });
+//   }
+//   if (upDate) {
+//     res.status(201).json(upDate);
+//   } else {
+//     res.status(500).json({
+//       errorMessage: "The user information could not be modified.",
+//     });
+//   }
+// });
+
 
 server.listen(8080, () => {
   console.log("server started");
